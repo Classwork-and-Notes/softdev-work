b26dc0e4f7435e946502d7ee04e394422d580849
boying44 <boying.tang.44@gmail.com>
2016-02-23 15:04:54 -0500

    growing and shrinking

--- hw01/animate.js
+++ hw01/animate.js
@@ -9,34 +9,37 @@ ctx.fillStyle = "#ff0000";
 
 var drawC = function(){
     console.log("drawn");
-
+    var radius = 0;
+    
     var animCode = function(){
-	var radius = 0;
+
 	ctx.clearRect(0, 0, c.width, c.height);
 	ctx.beginPath();
-	ctx.arc(250, 250, radius, 0, 2 * Math.PI);
+	ctx.arc(c.width/2, c.height/2, radius, 0, 2 * Math.PI);
 	ctx.fill();
-	radius += 1;
-	/*	
+
+	if (grow)
+	    radius += 1;
+	else
+	    radius -= 1;
+	console.log(radius);
+	
 	if (radius == c.width/2)
 	    grow = false;
 	if (radius == 0)
 	    grow = true;
 	
-	if (grow)
-	    radius += 1;
-	else
-	    radius -= 1;
-	*/
-    
+	console.log(grow)
+
 	window.requestAnimationFrame(animCode);
     }
     animCode();
 }
 
 var stopIt = function(){
-    windowCancelAnimation();
+    windowCancelAnimationFrame();
 }
 
 cbutton.addEventListener("click", drawC);
 
+sbutton.addEventListener("click", stopIt);
